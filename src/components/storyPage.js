62bf040fe74f127b9cee76a771c3d31a55bd8d9a
H. Cyrus Kia <hckia1@gmail.com>
2018-07-03 15:06:31 -0700

    cleaned up storyPage

--- src/components/storyPage.js
+++ src/components/storyPage.js
@@ -10,26 +10,26 @@ import AppFooter from './appFooter';
 class StoryPage extends Component {
   constructor(props) {
     super(props);
-    this.props.fetchAStory(this.props.match.params.story);
+    //this.props.fetchAStory(this.props.match.params.story);
   }
   componentDidMount(){
       // console.log(JSON.stringify(this.props.fetchStories()))
       console.log('params ', this.props.match.params.story);
       this.props.fetchAStory(this.props.match.params.story);
       console.log('Our... story? ',this.props.ourStory);
-      this.findSelectedStory = this.findSelectedStory.bind(this);
+      //this.findSelectedStory = this.findSelectedStory.bind(this);
   }
 
-  findSelectedStory(){
-    //this existed in a previous version as a way to map through an array of objects and find a speciifc story.
-    //i'm just using it now to try to find out what's going on.
-    console.log("Story we are trying to find - this props.ourStory ", this.props.ourStory);
-    return this.props.ourStory;
-  }
+  // findSelectedStory(){
+  //   //this existed in a previous version as a way to map through an array of objects and find a speciifc story.
+  //   //i'm just using it now to try to find out what's going on.
+  //   console.log("Story we are trying to find - this props.ourStory ", this.props.ourStory);
+  //   return this.props.ourStory;
+  // }
 
   render(){
-    console.log("Story we are trying to find - this props.ourStory ", this.props.ourStory);
-    const storyResults = this.findSelectedStory();
+    console.log("Story we are trying to find - this.props.ourStory ", this.props.ourStory);
+    const storyResults = this.props.ourStory; // use to be this.findSelectedStory(); . need to uncomment in componentDidMount to revive.
     console.log('Story found ', storyResults)
     return(
     <div>
@@ -40,7 +40,7 @@ class StoryPage extends Component {
       </div>
       <div className="story-group">
       <div className="story-left-container">
-      <Map location={storyResults.location} />
+        <Map location={storyResults.location} />
       </div>
       <div className="story-right-container">
       <p className="story-label">A story by... </p>
